23142656ff43073fdb256790085b83ba38c0951d
Fek <felixkern04@gmail.com>
2023-01-17 11:04:16 +0100

    installed voice & dependencies

--- events/ready.js
+++ events/ready.js
@@ -1,9 +1,12 @@
 const { Events } = require('discord.js');
+const { generateDependencyReport } = require('@discordjs/voice');
 
 module.exports = {
 	name: Events.ClientReady,
 	once: true,
 	execute(client) {
 		console.log(`Ready! Logged in as ${client.user.tag}`);
+
+		console.log(generateDependencyReport());
 	},
 };
